d0901deb944eec532a4a581a47ac1c018f9e79e4
Panayiotis11 <114312915+Panayiotis11@users.noreply.github.com>
2023-02-03 00:16:46 +0000

    Create Operatorspart.java

---
+++ tutorial1/Operatorspart.java
@@ -0,0 +1,34 @@
+package tutorial1;
+
+public class Operatorspart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 5;
+		int y = 7;
+		int z = 56;
+		int sum = x + y + z;
+		System.out.println(sum);
+		
+		double power = Math.pow(x, y); //5 to the power of 2
+		System.out.println(power);
+		
+		double u = x / (double)y; // this will print out the exact value of the calculation 
+		System.out.println(u);
+		
+		int j = z/y; //This will always output an integer only not a float
+		System.out.println(j);
+		
+		int k = 56 % 5; // & this will print out the remainder value
+		System.out.println(k);
+		
+		int g = 56 % 5; // & this will print out the remainder value
+		System.out.println(g);
+		
+		
+		
+
+	}
+
+}
