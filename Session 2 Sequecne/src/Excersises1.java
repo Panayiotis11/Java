f5d17d8f9d44331912aa3770a15aeb1fb6d2197b
Panayiotis11 <114312915+Panayiotis11@users.noreply.github.com>
2023-02-03 00:17:55 +0000

    Create Excersises1.java

---
+++ Session 2 Sequecne/src/Excersises1.java
@@ -0,0 +1,25 @@
+
+public class Excersises1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("////////////////////////////////");
+		System.out.println("==          Student Points            ==");
+		System.out.println("////////////////////////////////");
+		
+		System.out.println("Name       "  +   "Lab      "  +  "Bonus     " +  "Total   ");
+		System.out.println("----       " +    "---      "  +  "-----      " + "----");
+		
+		System.out.println("Joe         " +    "43         " + "7    "     +     "   50"     );
+		
+		System.out.println("William     " +    "50         " + "8    "     +     "   58"     );
+		
+		System.out.println("Mary Sue    " +    "39        " + "10    "     +     "   49"     );
+		
+		System.out.println("Panny       " +    "39    " + "    10    "     +     "   49"     );
+		
+		System.out.println("Panayiotis  " + "39        " + "10    "     +     "   49"     );
+	}
+
+}
