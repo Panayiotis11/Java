e36f3dffdd2be99cc7930ecf8228595fa2c8abd9
Panayiotis11 <114312915+Panayiotis11@users.noreply.github.com>
2023-02-09 16:22:13 +0000

    Add files via upload

---
+++ personaldetails.java
@@ -0,0 +1,25 @@
+
+public class personaldetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Name");
+		System.out.println("Address");
+		System.out.println("Age");
+		System.out.println("Phone number");
+		
+	System.out.println("     * ");
+	System.out.println("    ***  ");
+	System.out.println("   *****  ");
+	System.out.println("  *******  ");
+	System.out.println("   *****  ");
+	System.out.println("    ***  ");
+	System.out.println("     * ");
+	
+		
+		
+
+	}
+
+}
