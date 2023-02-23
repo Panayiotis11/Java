9197fd752fa29b3a85e9c4f24f21c74a5890bc4c
Panayiotis11 <114312915+Panayiotis11@users.noreply.github.com>
2023-02-23 00:27:54 +0000

    Update Loops.java

--- Loops/src/Loops.java
+++ Loops/src/Loops.java
@@ -1,10 +1,10 @@
+import java.util.Scanner;
 
 public class Loops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Hello");
+		System.out.println("11");
+		
 	}
-
 }
