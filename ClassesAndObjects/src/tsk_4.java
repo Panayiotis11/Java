d60e864da26b14e3f56d2653761fc8d70eea523d
Panayiotis11 <114312915+Panayiotis11@users.noreply.github.com>
2023-02-24 00:45:12 +0000

    Create tsk_4.java

---
+++ ClassesAndObjects/src/tsk_4.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class tsk_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Enter a 4 digit pin number to encrypt");
+		
+		int hexa;
+		
+		Scanner sc = new Scanner(System.in);
+		hexa = sc.nextInt();
+		
+		
+		
+//		System.out.println(Integer.toHexString(hexa));
+						
+		Random generator = new Random();
+	
+		int rand1;
+		int rand2;
+		
+		rand1 = generator.nextInt(65536) + 1000;
+		rand2 = generator.nextInt(65536) + 1000;
+		
+		
+		System.out.println("Your encrypted pin number is" + Integer.toHexString(rand1) + Integer.toHexString(rand2) );
+		
+		
+		
+
+	
+	}
+
+}
