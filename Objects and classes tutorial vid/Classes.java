e64463fcd96a83ab129db05a8b73b776f73eb0f3
Panayiotis11 <114312915+Panayiotis11@users.noreply.github.com>
2023-02-26 00:50:56 +0000

    Create Classes.java

---
+++ Objects and classes tutorial vid/Classes.java
@@ -0,0 +1,40 @@
+package tutorial1;
+
+public class Classes {
+
+	private String name;
+	private String food;
+	private String color;
+	private int age;
+	
+	
+	public Classes(String name, int age) {
+		this.name = name;
+		this.age = age;
+		
+	}
+	
+	public Classes(String food) {
+		this.food = food;
+		
+	}
+
+	
+	public void speak() {
+		System.out.print("My name is" + name + "and my age is " + age + "\n");
+		
+	}
+	
+	public int getAge() {
+		return this.age;
+		
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+		 
+}
+
+
