aa8fd3c90a260fd8fd00d2cc4e150642e2367f0e
Panayiotis11 <114312915+Panayiotis11@users.noreply.github.com>
2023-03-02 00:42:04 +0000

    Create tesstt.java

---
+++ tutorial 1/src/tutorial1/tesstt.java
@@ -0,0 +1,27 @@
+package tutorial1;
+import java.util.Scanner;
+
+public class tesstt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		int age = 15;
+
+		if(age<12 & age>20)
+
+		  System.out.println("join cubs");
+
+		else if (age>12 && age<16)
+
+		   System.out.println("join scouts");
+
+		else
+
+		   System.out.println("go to university or get a job");
+		
+	}
+
+}
