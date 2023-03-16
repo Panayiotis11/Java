b7d75f776600906ffcba528e28c742c8cca4888f
Panayiotis11 <114312915+Panayiotis11@users.noreply.github.com>
2023-03-16 01:17:03 +0000

    Create Bookshelf.java

---
+++ Parameters_methods/src/Bookshelf.java
@@ -0,0 +1,20 @@
+
+public class Bookshelf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Bookshelf bb = new Bookshelf();
+	}
+	
+	public Bookshelf() {
+		
+		float real;
+		int num1;
+		num1 = 10;
+		real = 1.2f;
+		num1 = (int) real;
+		System.out.println(num1);
+	}
+
+}
