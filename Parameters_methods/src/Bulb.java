01e1e317a7c5558eae6051261eb6774165f30ade
Panayiotis11 <114312915+Panayiotis11@users.noreply.github.com>
2023-03-16 01:17:01 +0000

    Create Bulb.java

---
+++ Parameters_methods/src/Bulb.java
@@ -0,0 +1,33 @@
+
+public class Bulb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+private Boolean light_bulb;	
+
+	public Bulb() {
+	
+	light_bulb = false;
+	
+	}
+	
+	public void turnOn() {
+	
+	light_bulb = true;
+	System.out.println("light bulb is on");
+		
+	}
+	
+	public void turnOff() {
+	
+	light_bulb = false;
+	System.out.println("light bulb is off");
+		
+		
+	}
+
+}
