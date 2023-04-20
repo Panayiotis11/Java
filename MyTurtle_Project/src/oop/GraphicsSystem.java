199347d21a91c5e1cda3b02b923c5e5211176dbf
Panayiotis11 <114312915+Panayiotis11@users.noreply.github.com>
2023-04-20 18:43:46 +0100

    Update GraphicsSystem.java

--- MyTurtle_Project/src/oop/GraphicsSystem.java
+++ MyTurtle_Project/src/oop/GraphicsSystem.java
@@ -1,5 +1,17 @@
 package oop;
 
-public class GraphicsSystem extends MainClass{
+import java.awt.FlowLayout;
 
+import javax.swing.JFrame;
+
+
+public class GraphicsSystem extends MainClass{
+    
+	public GraphicsSystem() {
+		
+		super();
+		
+		
+                                            
+       	}
 }
